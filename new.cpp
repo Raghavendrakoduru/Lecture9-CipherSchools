bc9851c264cc05e8cfd56d301e03039bd05ba693
Raghavendra Koduru <119060193+Raghavendrakoduru@users.noreply.github.com>
2024-06-09 12:36:35 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	cout<<"Ram";
+//	int a;
+//	cin>>a;
+//	int b[]={1,23,3};
+//	cout<<b[2];
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//int a[9];
+//cout<<"GIve 10 integers as input"<<endl;
+//for(int i=0;i<10;i++){
+//	cin>>a[i];
+//	
+//}	
+//cout<<"The 5 fiven inputs are";
+//for(int i=9;i>=0;i--){
+//	cout<<a[i]<<" ";
+//}
+//}
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=1;i<=20;i++)
+	{
+		cout<<i<<" ";
+	}
+}
